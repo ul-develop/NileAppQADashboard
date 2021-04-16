6faba1e30b95e08461b9a883d5ac4d88c3208265
lancodeenginestudio <lancodeenginestudio@unwiredlogic.com>
2021-04-16 05:10:14 +0000

    refs/heads/develop

--- docs/dashboard/property/service-worker.js
+++ docs/dashboard/property/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ec554c0755d2f71f6eaee189c7b9eaaa.js"
+  "./precache-manifest.e90a1415a82be5792bbdd2028aabdb63.js"
 );
 
 self.addEventListener('message', (event) => {
--- docs/dashboard/property/static/js/11.bcff82d6.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpnilehome=this.webpackJsonpnilehome||[]).push([[11],{1043:function(e,n,h){}}]);
-//# sourceMappingURL=11.bcff82d6.chunk.js.map
