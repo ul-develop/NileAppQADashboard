dea815dec2e9db4e64c2ca8186acc25fb4f5bd45
lancodeenginestudio <lancodeenginestudio@unwiredlogic.com>
2021-05-20 02:33:11 +0000

    refs/heads/epic/digioffer

--- docs/dashboard/forms/service-worker.js
+++ docs/dashboard/forms/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.7c370c61087600fa7e4f114241c25a62.js"
+  "./precache-manifest.9dbbe1a6fdf9f79562e2f3192e5d7e5c.js"
 );
 
 self.addEventListener('message', (event) => {
---
+++ docs/dashboard/precache-manifest.9dbbe1a6fdf9f79562e2f3192e5d7e5c.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d27aadb1202a140a1768aef45de0d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7549304a7cee5a501f1b",
+    "url": "./static/css/10.57b52b4d.chunk.css"
+  },
+  {
+    "revision": "37d5b49de1da617304ac",
+    "url": "./static/css/11.1331c91d.chunk.css"
+  },
+  {
+    "revision": "9ba427f318806470dc49",
+    "url": "./static/css/12.af90cd28.chunk.css"
+  },
+  {
+    "revision": "020e1567c57b2b38aff7",
+    "url": "./static/css/13.af90cd28.chunk.css"
+  },
+  {
+    "revision": "12d611c6a65b3fb810ca",
+    "url": "./static/css/14.cb7efdc7.chunk.css"
+  },
+  {
+    "revision": "2dd35f35c354c051335a",
+    "url": "./static/css/8.794145dd.chunk.css"
+  },
+  {
+    "revision": "f5110fdf9dd72e2ec30a",
+    "url": "./static/css/9.7878b521.chunk.css"
+  },
+  {
+    "revision": "58517b31bc0b5f4846b1",
+    "url": "./static/css/main.eddb2fa1.chunk.css"
+  },
+  {
+    "revision": "3ae59e6135c5eeac8f72",
+    "url": "./static/js/0.885abdb3.chunk.js"
+  },
+  {
+    "revision": "ce4481b4be9b9f244ab2",
+    "url": "./static/js/1.9a75f9f7.chunk.js"
+  },
+  {
+    "revision": "7549304a7cee5a501f1b",
+    "url": "./static/js/10.38a45373.chunk.js"
+  },
+  {
+    "revision": "37d5b49de1da617304ac",
+    "url": "./static/js/11.b9b11b24.chunk.js"
+  },
+  {
+    "revision": "9ba427f318806470dc49",
+    "url": "./static/js/12.15159e88.chunk.js"
+  },
+  {
+    "revision": "020e1567c57b2b38aff7",
+    "url": "./static/js/13.8f361138.chunk.js"
+  },
+  {
+    "revision": "12d611c6a65b3fb810ca",
+    "url": "./static/js/14.a1834fa1.chunk.js"
+  },
+  {
+    "revision": "18229c8b77f52fab6d89",
+    "url": "./static/js/15.23d63394.chunk.js"
+  },
+  {
+    "revision": "e74f4d182bdf9b47dc4e",
+    "url": "./static/js/16.2826c025.chunk.js"
+  },
+  {
+    "revision": "5451c771b2f361553088",
+    "url": "./static/js/17.46cabae8.chunk.js"
+  },
+  {
+    "revision": "971c8653400f82b64517",
+    "url": "./static/js/18.b3a23aaa.chunk.js"
+  },
+  {
+    "revision": "6560192deedf4dbef131",
+    "url": "./static/js/2.ae487790.chunk.js"
+  },
+  {
+    "revision": "26c6a8dd5fa6816f1a23",
+    "url": "./static/js/3.6b8f5629.chunk.js"
+  },
+  {
+    "revision": "1def7ba7f4f76d250a8b",
+    "url": "./static/js/4.9dfe8b8a.chunk.js"
+  },
+  {
+    "revision": "034e7a0816986b75e12f",
+    "url": "./static/js/5.4a8f470a.chunk.js"
+  },
+  {
+    "revision": "2dd35f35c354c051335a",
+    "url": "./static/js/8.e97f96cc.chunk.js"
+  },
+  {
+    "revision": "f5110fdf9dd72e2ec30a",
+    "url": "./static/js/9.a9fb8ebe.chunk.js"
+  },
+  {
+    "revision": "58517b31bc0b5f4846b1",
+    "url": "./static/js/main.b1c6ed0d.chunk.js"
+  },
+  {
+    "revision": "0d02e5462c17a70bd436",
+    "url": "./static/js/runtime-main.ded09c54.js"
+  },
+  {
+    "revision": "113fc76a624b189677219823dfde3b65",
+    "url": "./static/media/arrow_w.113fc76a.svg"
+  },
+  {
+    "revision": "9162fba254b4cd7cf0d54c30b7050958",
+    "url": "./static/media/drag.9162fba2.svg"
+  },
+  {
+    "revision": "890b0411c414e39d6bab002239645ab9",
+    "url": "./static/media/l_arrow.890b0411.svg"
+  },
+  {
+    "revision": "70515622aa44f0a2d7aca854ac66491e",
+    "url": "./static/media/logo.70515622.svg"
+  },
+  {
+    "revision": "a6f66507512011722a93c866336e3310",
+    "url": "./static/media/logo_w.a6f66507.svg"
+  },
+  {
+    "revision": "bee20da559d94ee312016ea8efcecd04",
+    "url": "./static/media/m_arrow.bee20da5.svg"
+  },
+  {
+    "revision": "cdf114c1528de7cb542be7ce24dc219f",
+    "url": "./static/media/marker.cdf114c1.svg"
+  }
+]);
