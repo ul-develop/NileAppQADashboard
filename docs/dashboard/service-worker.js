6dbb77fa8137c8e55aeff5896941ae725de366a3
lancodeenginestudio <lancodeenginestudio@unwiredlogic.com>
2021-06-17 07:30:21 +0000

    refs/heads/epic/digioffer

--- docs/dashboard/service-worker.js
+++ docs/dashboard/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8f4176f321d04b1c4be02f58b55f330e.js"
+  "./precache-manifest.03c373d4c1d4384c59df3400241d978d.js"
 );
 
 self.addEventListener('message', (event) => {
