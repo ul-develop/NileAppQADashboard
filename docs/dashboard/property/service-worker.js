80726dad5c745c896c44b224d34d03a85ee8c6c3
ces-ThaoPhan <ces-ThaoPhan@unwiredlogic.com>
2021-10-01 08:32:31 +0000

    refs/heads/epic/front-end

--- docs/dashboard/property/service-worker.js
+++ docs/dashboard/property/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.c4ffb1345d0e91d776230b7e8f763ecd.js"
+  "./precache-manifest.71c50c81020cc741f9d4168861c69783.js"
 );
 
 self.addEventListener('message', (event) => {
