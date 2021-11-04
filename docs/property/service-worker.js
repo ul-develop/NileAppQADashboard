0dcd1be944dd8d11d090fded9c30843c94d0c292
ces-lannguyen <ces-lannguyen@unwiredlogic.com>
2021-11-04 04:21:09 +0000

    refs/heads/hotfix/NILE-1800

--- docs/property/service-worker.js
+++ docs/property/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8bedaac7c1f8034c1b7bae2c6b45a49c.js"
+  "./precache-manifest.965dfc0558b4999608263a6b3b917d97.js"
 );
 
 self.addEventListener('message', (event) => {
