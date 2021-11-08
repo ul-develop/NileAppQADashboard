cd5a7b5d145edac851b80769ac990eea26173e33
ces-lannguyen <ces-lannguyen@unwiredlogic.com>
2021-11-08 09:27:36 +0000

    refs/heads/milestone/3

--- docs/offer/service-worker.js
+++ docs/offer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.965dfc0558b4999608263a6b3b917d97.js"
+  "./precache-manifest.8b3b088b46274b514a64df55e0af6767.js"
 );
 
 self.addEventListener('message', (event) => {
